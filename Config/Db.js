ba2902ab46dfd06cfc7c1a6e3f207214eb1593ea
Mujtaba <123797571+Mujtabaa-Ahmed@users.noreply.github.com>
2025-08-09 10:34:38 +0500

    Update Db.js

--- Config/Db.js
+++ Config/Db.js
@@ -8,9 +8,7 @@ async function DbConnection(){
     const connectionDB = await mongoose.connect(process.env.MONGODB_URI, {
   tls: true,
   tlsAllowInvalidCertificates: false, // only true if you're using self-signed certs
-  tlsInsecure: false,
-  tlsCAFile: '/path/to/ca.pem' // optional if needed
-})
+  tlsInsecure: false})
     if(connectionDB)  console.log("MONGO DB ATLAS IS CONNECTED SUCCESSFULLY")
 
     }
